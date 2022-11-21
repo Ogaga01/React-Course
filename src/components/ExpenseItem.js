ee1eb29a9b1049c88bed038d3421d2741249c8a2
Ogaga <iyaraogaga69@gmail.com>
2022-11-21 12:42:31 +0100

    Add props to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,23 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    return (
-      <div className='expense-item'>
-        <div>March 28th, 2021</div>
-        <div className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div className='expense-item__price'>$294.67</div>
-        </div>
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
       </div>
-    );
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
