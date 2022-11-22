9bf7565423c810f9a744d600d38e8f4ab7d42988
Ogaga <iyaraogaga69@gmail.com>
2022-11-22 21:53:23 +0100

    Refactor all components to function components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 
-function Expenses(props) {
+const Expenses = (props) => {
     return (
       <Card className='expenses'>
         <ExpenseItem
