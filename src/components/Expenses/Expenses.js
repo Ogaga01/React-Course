4f1d81c4e234876e7a6c828d1b8e826c8530eadc
Ogaga <iyaraogaga69@gmail.com>
2022-11-23 14:25:00 +0100

    Add expenses dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,27 +15,17 @@ const Expenses = (props) => {
     return (
       <div>
         <Card className="expenses">
-        <ExpensesFilter selected={year} onChangeExpenseYear={changeExpenseYear} />
-          <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-          ></ExpenseItem>
+          <ExpensesFilter
+            selected={year}
+            onChangeExpenseYear={changeExpenseYear}
+          />
+          {props.items.map((expense) => {
+            return <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />;
+          })}
         </Card>
       </div>
     );
