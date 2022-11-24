b7ab0873f6302211bbda2477d01ca4ac0c293c22
Ogaga <iyaraogaga69@gmail.com>
2022-11-24 08:32:27 +0100

    Conditional form rendering

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
+import { useState } from 'react'
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData)
+        setIsEditing(false)
+    }
+
+    const startEditing = () => {
+        setIsEditing(true)
+    }
+
+    const stopEditing = () => {
+        setIsEditing(false)
     }
 
     return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-        </div>
-    )
+      <div className="new-expense">
+        {isEditing ? (
+          <ExpenseForm onCancel={stopEditing} onSaveExpenseData={saveExpenseDataHandler} />
+        ) : (
+          <button type="button" onClick={startEditing}>
+            Add new expense
+          </button>
+        )}
+      </div>
+    );
 }
 
 export default NewExpense
