3a711b61b42d2de449357ea98eab2bae5230de9a
Ogaga <iyaraogaga69@gmail.com>
2022-11-29 10:44:10 +0100

    Add Card component

--- practice/src/Components/Users/AddUser.js
+++ practice/src/Components/Users/AddUser.js
@@ -1,3 +1,6 @@
+import styles from './AddUser.module.css'
+import Card from '../UI/Card'
+
 const AddUser = (props) => {
     const addUserHandler = (e) => {
         e.preventDefault()
@@ -5,13 +8,15 @@ const AddUser = (props) => {
     }
 
     return (
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
-            <button type="submit">Add User</button>
-      </form>
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" />
+          <button type="submit">Add User</button>
+        </form>
+      </Card>
     );
 }
 export default AddUser
