2593c769efd7525a5afd4b9170117183c7561a43
Ogaga <iyaraogaga69@gmail.com>
2022-12-08 14:59:37 +0100

    Create react app

--- pedrotechreact/episode1/src/App.js
+++ pedrotechreact/episode1/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name={'Ogaga'} age={25} description={'A fine, wealthy, healthy boy who God has got'} />
     </div>
+  );
+}
+
+const User = (props) => {
+  return (
+    <>
+      <hi>{props.name}</hi>
+      <h2>{props.age}</h2>
+      <p>{props.description}</p>
+    </>
   );
 }
 
