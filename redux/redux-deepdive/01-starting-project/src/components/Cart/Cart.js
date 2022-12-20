04921a6ddd33bd588701a5dd5abe81964cdedf22
Ogaga <iyaraogaga69@gmail.com>
2022-12-20 14:10:26 +0100

    Remove from cart

--- redux/redux-deepdive/01-starting-project/src/components/Cart/Cart.js
+++ redux/redux-deepdive/01-starting-project/src/components/Cart/Cart.js
@@ -1,15 +1,21 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartItems = useSelector((state)=>{return state.cartSlice.items})
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map((item)=>{return (
+          <CartItem
+            key={item.id}
+            item={{id: item.id, title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }}
+          />
+        );})}
       </ul>
     </Card>
   );
--- redux/redux-deepdive/01-starting-project/src/store/cart-slice.js
+++ redux/redux-deepdive/01-starting-project/src/store/cart-slice.js
@@ -12,12 +12,12 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => {
         return item.id === newItem.id;
       });
-          state.totalQuantity++
+        state.totalQuantity++
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
-          quantity: newItem.quantity,
+          quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title,
         });
@@ -29,8 +29,9 @@ const cartSlice = createSlice({
       removeFromCart(state, action) {
           const id = action.payload
           const existingItem = state.items.find((item) => { return item.id === id })
+          state.totalQuantity--
           if (existingItem.quantity === 1) {
-              state.items.filter((item)=>{return item.id !== id})
+              state.items = state.items.filter((item)=>{return item.id !== id})
           } else {
               existingItem.quantity--
               existingItem.totalPrice = existingItem.totalPrice - existingItem.price
