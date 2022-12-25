f36f86a3c1a54db6959ab33de50049cf4ccfbe6e
Ogaga <iyaraogaga69@gmail.com>
2022-12-25 15:24:42 +0100

    Fetch data redux

--- redux/redux-deepdive/01-starting-project/src/components/Cart/Cart.js
+++ redux/redux-deepdive/01-starting-project/src/components/Cart/Cart.js
@@ -1,21 +1,31 @@
-import { useSelector } from 'react-redux';
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  const cartItems = useSelector((state)=>{return state.cartSlice.items})
+  const cartItems = useSelector((state) => {
+    return state.cartSlice.items;
+  });
 
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        {cartItems.map((item)=>{return (
-          <CartItem
-            key={item.id}
-            item={{id: item.id, title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }}
-          />
-        );})}
+        {cartItems.map((item) => {
+          return (
+            <CartItem
+              key={item.id}
+              item={{
+                id: item.id,
+                title: item.name,
+                quantity: item.quantity,
+                total: item.totalPrice,
+                price: item.price,
+              }}
+            />
+          );
+        })}
       </ul>
     </Card>
   );
